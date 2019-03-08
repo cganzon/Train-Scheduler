37c48035cad988162b2533eac41c202eb310dfdd
cganzon <crganzon@gmail.com>
2019-03-08 15:17:08 -0800

    display user's input in table

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,17 +34,44 @@ $("#add-train").on("click", function (event) {
     // Uploading to firebase
     database.ref("/Train Data").push(newTrain);
 
-    console.log("==========================");
-    console.log("New train added:");
-    console.log(newTrain.trainName);
-    console.log(newTrain.destination);
-    console.log(newTrain.firstTrainTime);
-    console.log(newTrain.frequency);
-    console.log("==========================");
+    // console.log("==========================");
+    // console.log("New train added:");
+    // console.log(newTrain.trainName);
+    // console.log(newTrain.destination);
+    // console.log(newTrain.firstTrainTime);
+    // console.log(newTrain.frequency);
+    // console.log("==========================");
 
-    // Empyting input fields
+    // Emptying input fields after data is stored
     trainNameInput = $("#train-name").val("");
     destinationInput = $("#destination").val("");
     firstTrainTimeInput = $("#first-train-time").val("");
     frequencyInput = $("#frequency").val("");
+});
+
+// Every time a new train is added to the database
+database.ref("/Train Data").on("child_added", function(trainSnapshot) {
+    console.log(trainSnapshot.val());
+
+    var trainNameOutput =  trainSnapshot.val().trainName;
+    var destinationOutput = trainSnapshot.val().destination;
+    var firstTrainTimeOutput = trainSnapshot.val().firstTrainTime;
+    var frequencyOutput = trainSnapshot.val().frequency;
+
+    console.log("==========================");
+    console.log("New train added:");
+    console.log(trainNameOutput);
+    console.log(destinationOutput);
+    console.log(firstTrainTimeOutput);
+    console.log(frequencyOutput);
+    console.log("==========================");
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainNameOutput),
+        $("<td>").text(destinationOutput),
+        $("<td>").text(firstTrainTimeOutput),
+        $("<td>").text(frequencyOutput)
+    )
+
+    $("tbody").append(newRow);
 });
