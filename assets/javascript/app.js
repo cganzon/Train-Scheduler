efb3c818594bae6fe378d4dd14e76cb0ae53be8c
cganzon <crganzon@gmail.com>
2019-03-07 18:58:54 -0800

    linking files

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDXWYfcWZUH9a3XrfP_XyHerTD5S5NJYU4",
+    authDomain: "train-scheduler-b0101.firebaseapp.com",
+    databaseURL: "https://train-scheduler-b0101.firebaseio.com",
+    projectId: "train-scheduler-b0101",
+    storageBucket: "train-scheduler-b0101.appspot.com",
+    messagingSenderId: "815253003341"
+};
+firebase.initializeApp(config);
+// ==============================================
